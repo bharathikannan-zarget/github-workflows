6e06025a23339c84b4abb7ac36055578e249e58a
Bharathi Kannan R <bharathi.kannan@freshworks.com>
2020-05-11 00:56:05 +0530

    Testing google formatter

--- src/main/java/com/freshworks/githubflows/FileUtil.java
+++ src/main/java/com/freshworks/githubflows/FileUtil.java
@@ -1,52 +1,88 @@
 package com.freshworks.githubflows;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Collection;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOCase;
-import org.apache.commons.io.filefilter.DirectoryFileFilter;
-import org.apache.commons.io.filefilter.NotFileFilter;
-import org.apache.commons.io.filefilter.WildcardFileFilter;
 
-/** @author bk */
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
 public class FileUtil {
 
-  public static void listFiles(String dir) {
-    Collection<File> files =
-        FileUtils.listFiles(
-            new File(dir),
-            new WildcardFileFilter("*.txt", IOCase.SENSITIVE),
-            new NotFileFilter(DirectoryFileFilter.DIRECTORY));
+    public static final String[] ALPHA = {"a", "b", "c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q",
+    "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
-    files.stream().forEach(System.out::println);
-  }
+    public static final Random RANDOM = new Random();
 
-  public static void print(String str) {
-    System.out.println("Hello");
-  }
+    public static final long MAX_SIZE = 5 * 1000 * 1000; //10 MB
 
-  public static void copyFile(File file1, File file2) throws IOException {
-
-    FileUtils.copyFile(file1, file2);
-
-    if (FileUtils.contentEquals(file1, file2)) {
-
-      System.out.println("The files have equal content");
-    } else {
-
-      System.out.println("The files do not have equal content");
+    public static String getFileNameFromFilePath(String filePath) {
+        return filePath.substring(filePath.lastIndexOf("/") + 1);
     }
 
-    File docs = new File("src/main/resources/docs");
-    FileUtils.forceMkdir(docs);
-
-    FileUtils.copyFileToDirectory(file2, docs);
-  }
-
-  public void deleteFile(File f) {
-    if (f.exists()) {
-      f.delete();
+    public static boolean isValidImageFile(String contentType) {
+        if (!(contentType.equals("image/pjpeg") || contentType.equals("image/jpeg") || contentType.equals("image/png")
+                || contentType.equals("image/bmp")
+                || contentType.equals("image/x-png") || contentType.equals("image/x-icon"))) {
+            return false;
+        }
+        return true;
     }
-  }
+
+
+    public static List<ValidationError> validateFile(MultipartFile file) {
+        List<ValidationError> errors = new ArrayList<>();
+
+       if(file.getSize() > MAX_SIZE) {
+           errors.add(new ValidationError("File size should be less than : "+ MAX_SIZE));
+       }
+
+       if(!FileUtil.isValidImageFile(file.getContentType())) {
+            errors.add(new ValidationError("Invalid file format, only images are supported. File: " + file.getContentType()));
+       }
+
+       return errors;
+    }
+
+
+    @AllArgsConstructor
+    public static class ValidationError {
+        String msg;
+    }
+
+
+    public static String getRandomName(String fileExt) {
+        return _random() + "." + fileExt;
+    }
+
+    private static final String _random() {
+        String name = "photo-" + System.currentTimeMillis() + "-";
+
+        for(int i=0; i < 10; i++) {
+            name += ALPHA[RANDOM.nextInt(ALPHA.length-1)];
+        }
+        return name;
+    }
+
+    public static String getRandomName() {
+        return _random();
+    }
+
+
+    public static String readInputStream(InputStream io) throws IOException {
+
+        byte[] buffer = new byte[8092];
+        int bytesRead = -1;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+                while((bytesRead = io.read(buffer)) > 0) {
+                    out.write(buffer, 0, bytesRead);
+                }
+        } finally {
+            io.close();
+        }
+        return new String(out.toByteArray(), "utf-8");
+    }
+
 }
+
